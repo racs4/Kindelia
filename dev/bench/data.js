815dd1e41f4c2a1bff07f57b0696bbfe00e5e9c8
github-action-benchmark <github@users.noreply.github.com>
2022-10-13 20:14:50 +0000

    add Rust Benchmark (cargo) benchmark result for cddebf83d8e468135bd6b7a02577964153325a17

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1665692090249,
+  "repoUrl": "https://github.com/Kindelia/Kindelia",
+  "entries": {
+    "Rust Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "rheidner.achiles@gmail.com",
+            "name": "rheidner",
+            "username": "racs4"
+          },
+          "committer": {
+            "email": "rheidner.achiles@gmail.com",
+            "name": "rheidner",
+            "username": "racs4"
+          },
+          "distinct": true,
+          "id": "cddebf83d8e468135bd6b7a02577964153325a17",
+          "message": "add github action for continuous benchmark",
+          "timestamp": "2022-10-13T17:08:12-03:00",
+          "tree_id": "fbc499f2aa10c989a5ee6276b5883133f079525d",
+          "url": "https://github.com/Kindelia/Kindelia/commit/cddebf83d8e468135bd6b7a02577964153325a17"
+        },
+        "date": 1665692089644,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "test",
+            "value": 0,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
