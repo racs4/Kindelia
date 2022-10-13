abdc7c228c472b246fd71c2ee4fea36f3153eaab
rheidner <rheidner.achiles@gmail.com>
2022-10-13 12:07:00 -0300

    add bench ci

--- benches/bench.rs
+++ benches/bench.rs
@@ -9,7 +9,7 @@ use test::Bencher;
 #[bench]
 fn tree(b: &mut Bencher) {
   b.iter(|| {
-    std::thread::sleep(std::time::Duration::from_millis(1000));
+    std::thread::sleep(std::time::Duration::from_millis(10));
   })
 }
 
