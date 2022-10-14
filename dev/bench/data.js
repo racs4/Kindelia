9110e97d3c36531892cc38c997ca7a0c41bb7d8e
github-action-benchmark <github@users.noreply.github.com>
2022-10-14 05:05:17 +0000

    add Rust Benchmark (cargo) benchmark result for e46173f51e03991c9527fda74022e7b2ef6c1b5f

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1665723917781,
+  "repoUrl": "https://github.com/racs4/Kindelia",
+  "entries": {
+    "Rust Benchmark": {
+      "e46173f51e03991c9527fda74022e7b2ef6c1b5f": {
+        "commit": {
+          "author": {
+            "email": "rheidner.achiles@gmail.com",
+            "name": "rheidner",
+            "username": "racs4"
+          },
+          "committer": {
+            "email": "rheidner.achiles@gmail.com",
+            "name": "rheidner",
+            "username": "racs4"
+          },
+          "distinct": true,
+          "id": "e46173f51e03991c9527fda74022e7b2ef6c1b5f",
+          "message": "testing",
+          "timestamp": "2022-10-14T01:59:29-03:00",
+          "tree_id": "6f6a8cbec149252c45c487a11bc03bd0c7327482",
+          "url": "https://github.com/racs4/Kindelia/commit/e46173f51e03991c9527fda74022e7b2ef6c1b5f"
+        },
+        "date": 1665723917123,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "test",
+            "value": 0,
+            "range": "± 0",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    }
+  }
+}
